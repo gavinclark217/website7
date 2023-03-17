9a034b143c117cbce2aa16b8320a753f358204c6
Gavin Clark <111453652+gavinclark217@users.noreply.github.com>
2023-03-17 21:53:47 +0000

    Fri, Mar 17, 2023, 9:53 PM +00:00

--- js/sounds.js
+++ js/sounds.js
@@ -3,6 +3,10 @@ const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong']
 sounds.forEach(sound => {
     const btn = document.createElement('button')
     btn.classList.add('btn')
-    btn.i
+    btn.innerText = sound
+    btn.addEventListener('click', () => {
+        document.getElementById(sound).play()
+    })
+    document.getElementById('buttons').appendChild(btn)
 })
 
