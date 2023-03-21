189c8ebb48cd8bb5eb61b8e2ce18e06dccb42d7f
Gavin Clark <111453652+gavinclark217@users.noreply.github.com>
2023-03-21 21:46:05 +0000

    Tue, Mar 21, 2023, 9:46 PM +00:00

--- js/counter.js
+++ js/counter.js
@@ -2,4 +2,11 @@ const counters = document.querySlectorAll('.counter')
 
 counters.forEach(counter => {
     counter.innerText = '0'
+
+    const updateCounter = () => {
+        const target = counter.getAttribute('data-target')
+        console.log(target)
+    }
+
+    updateCounter()
 })
