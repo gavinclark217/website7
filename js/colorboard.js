3f5af051f5533de80563063dccda19630fe2ead6
Gavin Clark <111453652+gavinclark217@users.noreply.github.com>
2023-03-23 21:08:32 +0000

    Thu, Mar 23, 2023, 9:08 PM +00:00

--- js/colorboard.js
+++ js/colorboard.js
@@ -1,4 +1,4 @@
-const container = document.getElementById(container)
+const container = document.getElementById('container')
 const colors = ['#e74c3c', '#8e44ad', '#3498db', '#e67e22', '#2ecc71']
 const SQUARES = 500
 
@@ -6,4 +6,11 @@ for (let i = 0; i < SQUARES; i++) {
     const square = document.createElement('div')
     square.classList.add('square')
     container.appendChild(square)
+    square.addEventListener('mouseover', () => {
+        setColor(square)
+    })
+}
+
+function setColor(element) {
+    element.style.background = "yellow"
 }
