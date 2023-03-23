e00b48ef9c629ebc8e7e018ee8f0b4f14c7d5dbc
Gavin Clark <111453652+gavinclark217@users.noreply.github.com>
2023-03-23 21:15:01 +0000

    Thu, Mar 23, 2023, 9:15 PM +00:00

--- js/colorboard.js
+++ js/colorboard.js
@@ -1,5 +1,5 @@
 const container = document.getElementById('container')
-const colors = ['#e74c3c', '#8e44ad', '#3498db', '#e67e22', '#2ecc71']
+const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple']
 const SQUARES = 500
 
 for (let i = 0; i < SQUARES; i++) {
@@ -9,9 +9,16 @@ for (let i = 0; i < SQUARES; i++) {
     square.addEventListener('mouseover', () => {
         setColor(square)
     })
+    square.addEventListener('mouseout', () => {
+        removeColor(square)
+    })
 }
 
 function setColor(element) {
-    const color = colors[Math.floor(Math.random() * 5)]
+    const color = colors[Math.floor(Math.random() * 6)]
     element.style.background = color
+}
+
+function removeColor(element) {
+    element.style.background = '#2f2f2f'
 }
