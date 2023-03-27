a5800895cd14bbc0a47a25e52e18d1c0a775bc10
Gavin Clark <111453652+gavinclark217@users.noreply.github.com>
2023-03-27 22:05:57 +0000

    Mon, Mar 27, 2023, 10:05 PM +00:00

--- js/typing.js
+++ js/typing.js
@@ -1,6 +1,6 @@
-const textE1 - document.getElementById('text')
+const textE1 = document.getElementById('text')
 const speedE1 = document.getElementById('speed')
-const text - 'We Love Programming!'
+const text = 'We Love Programming!'
 let idx = 1
 let speed = 300 / speedE1.val
 
