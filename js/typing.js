d1193dadc49fba44537ea1e8bdb549e7d1433e0a
Gavin Clark <111453652+gavinclark217@users.noreply.github.com>
2023-04-13 20:41:23 +0000

    Thu, Apr 13, 2023, 8:41 PM +00:00

--- js/typing.js
+++ js/typing.js
@@ -2,7 +2,7 @@ const textE1 = document.getElementById('text')
 const speedE1 = document.getElementById('speed')
 const text = "We Love Programming!"
 let idx = 1
-let speed = 300 / speedE1.val
+let speed = 300 / speedE1.value
 
 speedE1.addEventListener('input', (element) => {
     speed = 300 / element.target.value
@@ -10,7 +10,7 @@ speedE1.addEventListener('input', (element) => {
 
 writeText()
 
-function writeText() {
+function writeText(){
     textE1.innerText = text.slice(0,idx)
     idx++
 
